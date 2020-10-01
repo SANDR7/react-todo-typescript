6b8e64a95c4bf69d77bafe6ca62d16910f22e271
Sander van Ast <sandervanast1@gmail.com>
2020-10-01 20:44:05 +0200

    :rocket: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoListItem } from "./TodoListItem";
+import { Todo } from './types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const todos: Array<Todo> = [
+  { text: "walk the dog", complete: true },
+  { text: "Write app", complete: false }
+];
+
+const App: React.FC = () => {
+  return ( 
+  <React.Fragment>
+    <TodoListItem todo={todos[0]} />
+    <TodoListItem todo={todos[1]} />
+  </React.Fragment>
   );
 }
 
